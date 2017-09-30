d17e2d82d2c601a375b98acd52694318ee4fd535
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-30 23:36:17 +0530

    Skip spaces

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!$%&*+-/:<=?>@^_~#"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
 
 main :: IO ()
 main = do
-        putStrLn "Enter name: "
-        name <- getLine
-        putStrLn ("Hello, " ++ name)
+        putStrLn "Enter expr: "
+        expr <- getLine
+        putStrLn (readExpr expr)
