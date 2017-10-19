f762ba8b83c34c3b3bd0ef6c8119eced9ba697a5
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-10-19 21:55:16 +0530

    Implement a repl

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Control.Monad
 import Control.Monad.Error
 import System.Environment
+import System.IO
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 data LispVal = Atom String
@@ -264,9 +265,27 @@ readExpr input = case parse parseExpr "lisp" input of
     Left err -> throwError $ Parser err
     Right val -> return val
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+    result <- prompt
+    if pred result
+        then return ()
+        else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
-main = do
-        putStrLn "Enter expr: "
-        expr <- getLine
-        evaled <- return $ liftM show $ (readExpr expr) >>= eval
-        putStrLn $ extractValue $ trapError evaled
+main = runRepl
